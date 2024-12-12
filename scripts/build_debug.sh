b9c01fa697b8d137a3e0be1cfb1b78d3fa888b9f
Truong Ngo <trg1432001.tu@gmail.com>
2024-12-12 10:52:54 +0700

    Add build_debug.sh

---
+++ scripts/build_debug.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Stop if meeting error
+set -e 
+
+# Flutter Build APK Script
+echo "=== Building Flutter Debug ==="
+
+# Clean old build
+flutter clean
+
+# Installing dependencies
+flutter pub get
+
+# Generating localization files
+flutter gen-l10n
+
+# Build APK
+if flutter run; then
+    echo "Building debug successfully!"
+else
+    echo "Error: Unable to build debug."
+    exit 1
+fi
